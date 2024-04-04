0303a03946be283c6757b5aa420feb51b79a2648
Jørn Alraun <ja@urbn-pockets.com>
2024-04-04 16:30:35 +0200

    Update MbitMoreServiceDAL.h

--- MbitMoreServiceDAL.h
+++ MbitMoreServiceDAL.h
@@ -79,6 +79,10 @@ public:
   // Buffer of characteristic for sending analog input values of P2.
   uint8_t analogInP2ChBuffer[MM_CH_BUFFER_SIZE_ANALOG_IN] = {0};
 
+// Buffer of characteristic for sending analog input values of P3.
+  uint8_t analogInP3ChBuffer[MM_CH_BUFFER_SIZE_ANALOG_IN] = {0};
+
+
 private:
   /**
    * @brief micro:bit runtime object.
@@ -94,12 +98,12 @@ private:
 
   GattCharacteristic *commandCh;
   GattCharacteristic *stateCh;
-  GattCharacteristic *directionCh;
   GattCharacteristic *pinEventCh;
   GattCharacteristic *actionEventCh;
   GattCharacteristic *analogInP0Ch;
   GattCharacteristic *analogInP1Ch;
   GattCharacteristic *analogInP2Ch;
+  GattCharacteristic *analogInP3Ch;
 };
 
 #endif // MBIT_MORE_SERVICE_DAL_H
